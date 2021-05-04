fbfe2f86140a1eb027ed601ad2ea3902913d8984
Jake Ralph <Jake@Jakes-MBP.attlocal.net>
2021-05-04 11:07:37 -0500

    update

--- memory.js
+++ memory.js
@@ -1,13 +1,17 @@
+// MemoryGame class to handle dynamic elements of game
 class MemoryGame {
-  constructor(deckSize, images, frontImage) {
-    this.frontImage = frontImage;
+    // construct memory game by creating a deck and adding one card w/ each image to deck
+  constructor(deckSize, images) {
     this.deck = new Deck();
+    // add two of each card from images to the deck
     for (let i = 0; i < deckSize / 2; i++) {
         this.deck.addCard(new Card(i, images[i]));
         this.deck.addCard(new Card(i, images[i]));
     }
     this.start();
   }
+
+  // assign a card in the deck to each div and add event listeners for clicks
   draw() {
     const cards = document.querySelectorAll(".memory-card");
     let backs = document.getElementsByClassName("back-face");
@@ -23,6 +27,8 @@ class MemoryGame {
       })
     );
   }
+  
+  // set/reset start conditions for memory game
   start() {
     this.deck.shuffle();
     this.draw();
@@ -30,17 +36,25 @@ class MemoryGame {
     this.clicked = [];
     this.updateTurns(true);
   }
+  // flip over any face up cards and reset the game with the start() function
   reset() {
     let that = this;
-    this.matched.forEach((card) => {card.flip();});
-    this.clicked.forEach((card) => {card.flip();});
+    this.matched.forEach((card) => {card.flip()});
+    this.clicked.forEach((card) => {card.flip()});
+    this.matched = [];
+    this.clicked = [];
     setTimeout(function () {that.start();}, 500);
   }
+  // evaluate the card clicked by user
   evalClick(that) {
     let newCard = this.deck.getCardById(that.id);
+    
+    // Ignore extra clicks
     if (newCard.faceUp || this.clicked.length === 2) return;
     this.clicked.push(newCard);
     newCard.flip();
+
+    // once two cards have been clicked, check for match/win conditions
     if (this.clicked.length === 2) {
       this.updateTurns();
       if (this.clicked[0].value != this.clicked[1].value) {
@@ -57,21 +71,27 @@ class MemoryGame {
       }
     }
   }
+
+  // update turn counter
   updateTurns(reset = false) {
     const turnScore = document.querySelector("#turns");
     if (reset) this.turns = 0;
     else this.turns++;
     turnScore.textContent = this.turns;
   }
+
+  // check if all cards are matched
   checkWin() {
-    if (this.matched.length === this.deck.length()) {
+      let that = this;
+    if (this.matched.length === this.deck.len()) {
       setTimeout(function () {
-        alert("You won!");
+        alert(`You won in ${that.turns} turns!`);
       }, 700);
     }
   }
 }
 
+// Deck class for MemoryGame
 class Deck {
   constructor() {
     this.cards = [];
@@ -88,11 +108,12 @@ class Deck {
   getCardById(id) {
     return this.cards.filter((card) => card.element.id === id)[0];
   }
-  length() {
+  len() {
     return this.cards.length;
   }
 }
 
+// Card class for MemoryGame
 class Card {
   constructor(value, image) {
     this.image = image;
@@ -104,6 +125,7 @@ class Card {
     this.faceUp = !this.faceUp;
     this.element.classList.toggle("flip");
   }
+  // Set html element for click handling
   setElement(element) {
     this.element = element;
   }
@@ -119,5 +141,4 @@ const imageArr = [
   "images/LOL.png",
 ];
 const deckSize = 16;
-
-let game = new MemoryGame(deckSize, imageArr, "images/Unicorn.png");
+const game = new MemoryGame(deckSize, imageArr);
